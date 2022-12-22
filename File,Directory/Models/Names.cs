bd949c1d296bd26ab61ed2557a5e77a6667c610a
Bahadirlinurlan <tu7mu42pa@code.edu.az>
2022-12-22 01:20:47 -0800

    first commit

---
+++ File,Directory/Models/Names.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Directory.Models
+{
+    internal class Names
+    {
+        static int _id;
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Names()
+        {
+            Id = _id;
+            _id++;
+        }
+
+        public static void AddMethod(string name, string path)
+        {
+            using(StreamWriter stream= new StreamWriter(path))
+            {
+                stream.WriteLine(path);
+            }
+        }
+        public static bool SearchMethod(string name, string path)
+        {
+            using(StreamReader stream= new StreamReader(path))
+            {
+                foreach(var item in path)
+                {
+                    Console.WriteLine("Axtarir siyahini");
+                    stream.ReadToEnd();
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ File,Directory/Program.cs
@@ -0,0 +1,22 @@
+﻿using File_Directory.Models;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization;
+
+namespace File_Directory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = Path.Combine(Path.Combine(Directory.GetCurrentDirectory(), @"../../../", "Names"));
+            Directory.SetCurrentDirectory(path);
+
+            List<Names> list = new List<Names>();
+
+            list.Add(new Names { Name = "Seymur"});
+           
+
+            path = Path.Combine(path, "names.json");
+        }
+    }
+}
